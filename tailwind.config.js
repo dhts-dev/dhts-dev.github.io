807e1630fa41d2fbc7f3f54b55ec8cb9aa27f41d
hitansh1926 <professionalplayers26@gmail.com>
2025-07-20 11:35:00 +0530

    Changed jobsData.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,15 @@ module.exports = {
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif']
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.8s ease-in-out'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' }
+        }
       }
     }
   },
