c8f7c9efbb5f9d4198af4d203da9b76d7172a108
hitansh1926 <professionalplayers26@gmail.com>
2025-07-28 13:55:09 +0530

    1) Some Formatting Changes 2) Removed Unnecessary Routes from routes.jsx 3) Created Sitemap.xml 4) Created robots.txt file

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,16 +5,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import JobSearch from './pages/JobSearch';
 import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
 import ResourcesSection from './pages/ResourcesSection';
 import EmployersServices from './pages/EmployersServices';
 import PostJobForm from './pages/JobSearch/PostJobForm';
 import CandidateApplicationForm from './pages/JobSearch/CandidateApplicationForm';
 import Industry from './pages/Industry';
-import OverseasJobs from './pages/JobSearch/OverseasJobs';
 import Login from './pages/LoginDialog';
 import Signup from './pages/Signup';
-import FeaturedJobsSection from './pages/Homepage/FeaturedJobsSection';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Gallery from './pages/Resources/Gallery';
 import Blogs from './pages/Resources/Blogs';
@@ -27,20 +24,15 @@ const AppRoutes = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/current-jobs" element={<JobSearch />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/job-seekers" element={<Services />} />
-        <Route path="/services/employers" element={<EmployersServices />} />
-        <Route path="/resources" element={<ResourcesSection />} />
+        {/* <Route path="/resources" element={<ResourcesSection />} />
         <Route path="/resources/gallery" element={<Gallery />} />
         <Route path="/resources/blogs" element={<Blogs />} />
         <Route path="/resources/language" element={<LanguageSelector />} />
-        <Route path="/post-job" element={<PostJobForm />} />
+        <Route path="/post-job" element={<PostJobForm />} /> */}
         <Route path="/candidate-application" element={<CandidateApplicationForm />} />
         <Route path="/industry" element={<Industry />} />
-        <Route path="/overseas-jobs" element={<OverseasJobs />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/FeaturedjobsSection" element={<FeaturedJobsSection />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
